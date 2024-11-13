0bc1da4c459444def3e448d88635bb04763c6ee8
Marcelle Andrade Pereira <92685909+Marcelleap@users.noreply.github.com>
2024-11-13 07:08:22 -0300

    Create Trafego.c

---
+++ Trafego.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (){
+
+    int torre; //Quem vai coordenar as operações
+    int piperequisicao [2];   //Requisicao dos avioes → torre 
+    int pipeprocessamento[2]; //Processamento de torre → aviao  
+
+    //Para criar o processo do aviao 
+    
+
+
+
+
+    return 0;
+}
